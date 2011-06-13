235401138772c15884a6ed6dba1c7a4dd3111aa3
simophin <simophin@LFC-LAPTOP>
2011-06-13 17:35:20 +0800

    Modify preferences in android
    Change-Id: Id5052b6a8533330928c8b85d7d4f934b58edcbfa

--- android/src/com/lfcinvention/RemoteVision/SettingActivity.java
+++ android/src/com/lfcinvention/RemoteVision/SettingActivity.java
@@ -1,192 +1,53 @@
 package com.lfcinvention.RemoteVision;
 
-import com.lfcinvention.RemoteVision.ServiceConfiguration.NetworkMode;
-
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.Layout;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.TableRow;
-import android.widget.TextView;
-import android.widget.TableLayout.LayoutParams;
-import android.widget.TextView.OnEditorActionListener;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
 
-public class SettingActivity extends Activity {
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-
-
+public class SettingActivity extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.setting);
-		setTitle(getString(R.string.setting_text));
-		mConfiguration = ServiceConfiguration.getCurrentConfiguration();
 		
-		mSpinner = (Spinner) findViewById(R.id.spinConnectMode);
-		mRowContent = (LinearLayout) findViewById(R.id.rowSettingContent);
-		mBtnSave = (Button) findViewById(R.id.btnSave);
-		mBtnCancel = (Button) findViewById(R.id.btnCancel);
+		addPreferencesFromResource(R.xml.preferences);
 		
-		mBtnSave.setOnClickListener(onBtnSaveClicked);
+		mNetworkModePreference = (ListPreference) findPreference("network_mode");
+		mNetworkModePreference.setOnPreferenceChangeListener(mOnNetworkModeChanged);
 		
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-				R.array.setting_connect_mode_string_array, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		mSpinner.setAdapter(adapter);
-
-		mSpinner.setOnItemSelectedListener(onNetworkModeSelected);
-		
-		
+		clearPreferences();
+		setEnablePreference(mNetworkModePreference.getValue() == "0" ? "server_mode" : "relay_mode");
 	}
 	
 	
-	
-	private void setupServerMode (ServiceConfiguration conf) {
-		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		View v = inflater.inflate(R.layout.setting_server_mode, null);
-		mRowContent.addView(v,0,new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-		
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-				R.array.setting_server_network_string_array, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		
-		Spinner networkSpinner = (Spinner) v.findViewById(R.id.spinnerServerNetworkMode);
-		networkSpinner.setAdapter(adapter);
-		networkSpinner.setSelection(conf.serverType);
-		
-		EditText portView = (EditText) findViewById(R.id.serverModePort);
-		portView.addTextChangedListener(new onValidateNeededTextWatcher());
-		if (conf.serverPort >= 0) {
-			portView.setText(String.valueOf(conf.serverPort));
-		}
-	}
-	
-	private void setupRelayMode (ServiceConfiguration conf) {
-		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		View v = inflater.inflate(R.layout.setting_relay_mode, null);
-		mRowContent.addView(v,0,new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-		
-		EditText serverAddr = (EditText) findViewById(R.id.relayServerHostEdit);
-		serverAddr.setText(conf.relayServerHost);
-		serverAddr.addTextChangedListener(new onValidateNeededTextWatcher());
-		
-		EditText port = (EditText) findViewById(R.id.relayServerPortEdit);
-		port.addTextChangedListener(new onValidateNeededTextWatcher());
-		port.setText(String.valueOf(conf.relayServerPort));
-		
-		
-	}
-	
-	private void clearMainSettingContent () {
-		mRowContent.removeAllViews();
-	}
-	
-	private OnItemSelectedListener onNetworkModeSelected = new OnItemSelectedListener() {
-
-		public void onItemSelected(AdapterView<?> parent, View vew, int pos,
-				long id) {
-			switch (pos) {
-			default:
-			case 0: // SERVER MODE
-			{
-				mConfiguration.networkMode = NetworkMode.SERVER;
-				clearMainSettingContent();
-				setupServerMode(mConfiguration);				
-				break;
+	private OnPreferenceChangeListener mOnNetworkModeChanged = new Preference.OnPreferenceChangeListener() {
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			String v = newValue.toString();
+			if (v.equals(String.valueOf(0))) {
+				clearPreferences();
+				setEnablePreference("server_mode");
+			}else {
+				clearPreferences();
+				setEnablePreference("relay_mode");
 			}
-			
-			case 1: // RELAY MODE
-			{
-				mConfiguration.networkMode = NetworkMode.RELAY_PROVIDER_CLIENT;
-				clearMainSettingContent();
-				setupRelayMode(mConfiguration);
-				break;
-			}
-			}
-			
-			mBtnSave.setEnabled(validate());
-		}
-
-		public void onNothingSelected(AdapterView<?> arg0) {
-			
+			return true;
 		}
 	};
 	
-	private class onValidateNeededTextWatcher implements TextWatcher {
-		public void afterTextChanged(Editable s) {
-		
-		}
-
-		public void beforeTextChanged(CharSequence s, int start, int count,
-				int after) {
-			
-		}
-
-		public void onTextChanged(CharSequence s, int start, int before,
-				int count) {
-			mBtnSave.setEnabled(validate());	
-		}
+	private void clearPreferences() {
+		Preference sp = findPreference("server_mode");
+		Preference rp = findPreference("relay_mode");
+		sp.setEnabled(false);
+		rp.setEnabled(false);
 	}
 	
-	private OnClickListener onBtnSaveClicked = new OnClickListener() {
-		public void onClick(View v) {
-			
-		}
-	};
-	
-	private boolean validate () {
-		boolean ret = true;
-		switch( mConfiguration.networkMode) {
-		case NetworkMode.SERVER: {
-			break;
-		}
-		
-		case NetworkMode.RELAY_PROVIDER_CLIENT: {
-			TextView hostEdit = (TextView) findViewById(R.id.relayServerHostEdit);
-			TextView portEdit = (TextView) findViewById(R.id.relayServerPortEdit);
-			if (hostEdit.getText().toString().trim().length() < 1) {
-				return false;
-			}
-			if (portEdit.getText().toString().trim().length() < 1) {
-				return false;
-			}
-			if ( Integer.parseInt(portEdit.getText().toString()) <= 0) {
-				return false;
-			}
-			break;
-		}
-		
-		default: ret = false; break;
-		}
-		
-		
-		
-		return ret;
+	private void setEnablePreference (String key) {
+		Preference s = findPreference(key);
+		s.setEnabled(true);
 	}
 
 	
-	private Button  mBtnSave = null, mBtnCancel = null;
-	private Spinner mSpinner = null;
-	private LinearLayout mRowContent = null;
-	private ServiceConfiguration mConfiguration = null;
+	ListPreference mNetworkModePreference = null;
 }
